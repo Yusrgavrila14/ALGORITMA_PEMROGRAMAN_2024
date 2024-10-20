f703f43208ba1bce78cdb4799add556588a7056c
Yusrgavrila14 <yurs696@gmail.com>
2024-10-20 20:37:00 +0800

    Add files via upload

---
+++ latihan 1 modul 3.py
@@ -0,0 +1,8 @@
+bilangan = float(input("Masukkan sebuah bilangan: "))
+
+if bilangan > 0:
+    print("Bilangan tersebut adalah bilangan positif.")
+elif bilangan < 0:
+    print("Bilangan tersebut adalah bilangan negatif")
+else:
+    print("Bilangan tersebut adalah nol.")
